034c263483c0c263d71ce3efab04a9a6bf28eb00
Ubuntu <ubuntu@vps-95f47936.vps.ovh.net.novalocal>
2025-05-20 13:34:32 +0000

    7.1

---
+++ AldafluxSeoBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Aldaflux\Bundle\SeoBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AldafluxSeoBundle extends Bundle
+{
+}
---
+++ Twig/SeoExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Aldaflux\Bundle\SeoBundle\Twig;
+ 
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+use Aldaflux\Bundle\SeoBundle\Service\SeoService;
+
+/**
+ * Description of SeoExtension.
+ *
+ * @author: leogout
+ */
+class SeoExtension extends AbstractExtension
+{
+    
+    private $seoService;
+    
+    
+    /**
+     * @var SeoGeneratorProvider
+     */
+    protected $generatorProvider;
+
+    /**
+     * SeoExtension constructor.
+     *
+     * @param SeoGeneratorProvider $generatorProvider
+     */
+    public function __construct( SeoService $seoService)
+    {
+        $this->seoService = $seoService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions() : array
+    {
+        return array(
+            new TwigFunction('aldaflux_seo', [$this->seoService, 'renderMeta'], ['is_safe' => ['html']]),
+        );
+    }
+ 
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'aldaflux_seo.twig.seo_extension';
+    }
+}
